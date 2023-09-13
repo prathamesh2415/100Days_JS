cfb4d531b39f1baa6fceedc90c371f4b49b26535
Prathamesh Mule <40546410+prathamesh2415@users.noreply.github.com>
2023-09-13 18:17:07 +0000

    initial commit

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,19 @@
+const accountId=1000
+let accountEmail = "prathamesh@gmail.com"
+var accountPassword = "12345"
+
+accountCity ="Mumbai"
+// accountId =2  //not allowed
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.table([accountEmail,accountPassword,accountCity])
+//after change
+
+
+accountEmail = "hhhh@gmail.com"
+accountPassword = "4567"
+accountCity= "Bengaluru"
+
+console.table([accountEmail,accountPassword,accountCity])

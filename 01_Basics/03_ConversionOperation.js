538901dfe07387f15562c8ad443bb54e1371eda9
Prathamesh Mule <40546410+prathamesh2415@users.noreply.github.com>
2023-09-14 07:48:20 +0000

    Coversion

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33AVB"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumer = Number(score)
+console.log(typeof valueInNumer);
+console.log(valueInNumer)
+
+
+//Notes
+
+//"33" => 33
+//"33abc" => Nan
+//true => 1; false => 0
+//null => 0
+//undefined =>Nan
+//"Pratamesh" => Nan
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true 0=> false
+//"" =>false
+//"hitesh" => true
+

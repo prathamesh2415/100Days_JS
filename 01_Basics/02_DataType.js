1e8f4a40f86296e72129b1c7251581ceaf3fcb14
Prathamesh Mule <40546410+prathamesh2415@users.noreply.github.com>
2023-09-14 07:16:02 +0000

    datatypes

---
+++ 01_Basics/02_DataType.js
@@ -0,0 +1,12 @@
+"Use Strict"; //treat all js Code as newer version
+
+console.log("Prathamesh")
+
+let name = "Prathamesh"
+let age =25
+let isLoggedIn = false
+
+//alert(3+3)  //we are using nodejs, not browser
+
+console.log(typeof null);  //object
+console.log(typeof undefined); //undefined

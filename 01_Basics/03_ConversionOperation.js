d7cd1ff696330afb6fe2f1b23691c849db234b58
Prathamesh Mule <40546410+prathamesh2415@users.noreply.github.com>
2023-09-14 08:11:56 +0000

    operations

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -25,3 +25,16 @@ console.log(booleanIsLoggedIn);
 //"" =>false
 //"hitesh" => true
 
+//*************************  Operations  ******************************************************
+
+console.log("1"+2)              //12
+console.log(1+"2")              //12
+console.log("1"+2+2)            //122
+console.log(1+2+"2")            //32
+console.log(1+2+"2"+4)          //324
+console.log(+true)              //1
+console.log(+"")                //0
+
+let gameCounter =100
+++gameCounter;
+console.log(gameCounter);

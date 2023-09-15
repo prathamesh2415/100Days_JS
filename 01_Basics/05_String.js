64b9a21c9d06a438a2a1f5ff773d965e39761678
Prathamesh Mule <40546410+prathamesh2415@users.noreply.github.com>
2023-09-15 17:05:43 +0000

    String Operation

---
+++ 01_Basics/05_String.js
@@ -0,0 +1,35 @@
+const name = "Prathamesh"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Prathamesh-PM-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   Prathamesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://Prathamesh.com/Prahtamesh%24Mule"
+
+console.log(url.replace('%24', '-'))
+
+console.log(url.includes('sh'))
+
+console.log(gameName.split('-'));
